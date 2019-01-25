f636334bf93cd2c80d8c83d99edb96756da7131e
James <james.hong@tripsaas.com>
2019-01-25 16:29:11 +0800

    Initial Commit

---
+++ soba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soba
+//
+//  Created by James on 2019/1/25.
+//  Copyright © 2019年 tripresso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
